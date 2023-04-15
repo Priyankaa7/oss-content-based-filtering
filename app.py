c1d0678a443793bcd1558335da0cb334e3a150e2
Jagruti <jagrutirkm@gmail.com>
2023-04-15 20:47:22 +0530

    flask update

--- app.py
+++ app.py
@@ -1,55 +1,79 @@
+import pandas as pd
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from gensim.models import Word2Vec
+from gensim.utils import simple_preprocess
+from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-model = pickle.load(open('oss-content-based-filtering.pkl', 'rb'))
+cors = CORS(app)
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/oss',methods=['POST'])
-def oss():
-    '''
-    For rendering results on HTML GUI
-    '''
-    res = recommendation.results(request.args.get('name'))
-    # return jsonify(res)
+# CORS error handling
+
+"""Import the dataset"""
+
+df = pd.read_csv('oss_data.csv')
+
+"""Combining the different attributes of the dataset into a single string"""
+
+df['content'] = df['name'].astype(str) + ' ' + df['desc'].astype(str) + ' ' + df['tags'] + ' ' + df['upforgrabs__link'].astype(str)
+df['content'] = df['content'].fillna('')
+df['content']
+
+"""Tokenize content for Word2Vec"""
+
+df['tokenized_content'] = df['content'].apply(simple_preprocess)
+df['tokenized_content']
+
+"""Training the Word2Vec model"""
+
+model = Word2Vec(vector_size=100, window=5, min_count=1, workers=4)
+model.build_vocab(df['tokenized_content'])
+model.train(df['tokenized_content'], total_examples=model.corpus_count, epochs=10)
+
+"""Function to average word vectors for a text"""
+
+def average_word_vectors(words, model, vocabulary, num_features):
+    feature_vector = np.zeros((num_features,), dtype="float64")
+    nwords = 0.
     
-    # int_features = [int(x) for x in request.form.values()]
-    # final_features = [np.array(int_features)]
-    # prediction = model.predict(final_features)
+    for word in words:
+        if word in vocabulary: 
+            nwords = nwords + 1.
+            feature_vector = np.add(feature_vector, model.wv[word])
+    
+    if nwords:
+        feature_vector = np.divide(feature_vector, nwords)
+        
+    return feature_vector
 
-    # output = round(prediction[0], 2)
+"""Function to compute average word vectors for all repos"""
 
-    return render_template('index.html', oss_text='Repository: $ {}'.format(res))
+def averaged_word_vectorizer(corpus, model, num_features):
+    vocabulary = set(model.wv.index_to_key)
+    features = [average_word_vectors(tokenized_sentence, model, vocabulary, num_features) for tokenized_sentence in corpus]
+    return np.array(features)
 
-@app.route('/oss_api',methods=['POST'])
-def oss_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    res = recommendation.results(request.args.get('name'))
-    # prediction = model.predict([np.array(list(data.values()))])
-    # output = prediction[0]
-    return jsonify(res)
+"""Compute average word vectors for all repos"""
 
-if __name__ == "__main__":
+w2v_feature_array = averaged_word_vectorizer(corpus=df['tokenized_content'], model=model, num_features=100)
+
+@app.route('/similar_repos', methods=['POST'])
+def get_similar_repos():
+    user_oss = request.json['query']
+    oss_index = df[df['name'] == user_oss].index[0]
+    user_oss_vector = w2v_feature_array[oss_index].reshape(1, -1)
+    similarity_scores = cosine_similarity(user_oss_vector, w2v_feature_array)
+
+    similar_repos = list(enumerate(similarity_scores[0]))
+    sorted_similar_repos = sorted(similar_repos, key=lambda x: x[1], reverse=True)[1:11]
+    result = [{'name': df.loc[i, 'name'], 'similarity_score': score,'desc':df.loc[i,'desc'], 'tags':df.loc[i,'tags'], 'link':df.loc[i,'upforgrabs__link']} for i, score in sorted_similar_repos]
+
+    return jsonify(result)
+
+if __name__ == '__main__':
     app.run(debug=True)
-
-# from flask import Flask,request,jsonify
-# from flask_cors import CORS 
-# import recommendation
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/oss-content-based-filteration', methods=['POST'])
-# def recommend_oss():
-#     res = recommendation.results(request.args.get('name'))
-#     return jsonify(res)
-
-# if __name__=='__main__':
-#     app.run(port=5000, debug = True)
--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/oss_api'
-r = requests.post(url,json={'name':100})
-
-print(r.json())
