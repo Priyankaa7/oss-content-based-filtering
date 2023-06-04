ab54c6ac77dee0ceee28aab76d70099f3e7d67cf
Jagruti <jagrutirkm@gmail.com>
2023-06-05 01:12:51 +0530

    model update

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ df.fillna(method='ffill', inplace=True)
 """Combining the different attributes of the dataset into a single string"""
 
 df['content'] = df['name'].astype(str) + ' ' + df['desc'].astype(
-    str) + ' ' + df['tags'] + ' ' + df['site'].astype(str)
+    str) + ' ' + df['tags'] + ' ' + df['site'].astype(str) + ' ' + df['upforgrabs__link'].astype(str) + ' ' + df['stats__issue-count'].astype(str)
 df['content'] = df['content'].fillna('')
 df['content']
 
@@ -74,50 +74,43 @@ w2v_feature_array = averaged_word_vectorizer(
 
 @app.route('/similar_repos', methods=['POST'])
 def get_similar_repos():
-    
-    # user_oss = request.json['query']
-    # oss_index = df[df['name'] == user_oss].index[0]
-    # user_oss_vector = w2v_feature_array[oss_index].reshape(1, -1)
-    # similarity_scores = cosine_similarity(user_oss_vector, w2v_feature_array)
 
-    # similar_repos = list(enumerate(similarity_scores[0]))
-    # sorted_similar_repos = sorted(
-    #     similar_repos, key=lambda x: x[1], reverse=True)[1:11]
-    # result = [{'name': df.loc[i, 'name'], 'similarity_score': score, 'desc':df.loc[i, 'desc'],
-    #            'tags':df.loc[i, 'tags'], 'link':df.loc[i, 'upforgrabs__link']} for i, score in sorted_similar_repos]
-
-    # return jsonify(result)
-
-    user_oss = request.json['query'].strip().replace(" ", "") 
+    user_oss = request.json['query'].strip().replace(" ", "")
 
     # accept input with spaces and remove spaces
     oss_index = np.nan
-    if ((df['tags'] == user_oss).any()): 
+    if ((df['tags'] == user_oss).any()):
         oss_index = df.loc[df['tags'] == user_oss].index[0]
     else:
-        oss_index = df.loc[df['name'] == user_oss].index[0] if ((df['name'] == user_oss).any()) else np.nan
+        oss_index = df.loc[df['name'] == user_oss].index[0] if (
+            (df['name'] == user_oss).any()) else np.nan
 
     if not np.isnan(oss_index):
         # Compute the cosine similarities between the user repo and all other repo
         user_oss_vector = w2v_feature_array[oss_index].reshape(1, -1)
-        similarity_scores = cosine_similarity(user_oss_vector, w2v_feature_array)
+        similarity_scores = cosine_similarity(
+            user_oss_vector, w2v_feature_array)
 
         # Get the top 20 most similar repos
         similar_repos = list(enumerate(similarity_scores[0]))
-        sorted_similar_repos = sorted(similar_repos, key=lambda x: x[1], reverse=True)[:30]
+        sorted_similar_repos = sorted(
+            similar_repos, key=lambda x: x[1], reverse=True)[:30]
 
         # Print the top 20 similar repos
-        res = [] 
-        printed_names = [] # List to keep track of printed names
+        res = []
+        printed_names = []  # List to keep track of printed names
         for i, score in sorted_similar_repos:
             name = df.loc[i, 'name']
-            if name not in printed_names: # Check if name has already been printed
+            if name not in printed_names:  # Check if name has already been printed
                 tags = df.loc[i, 'tags']
                 link = df.loc[i, 'site']
                 desc = df.loc[i, 'desc']
+                upforgrabs__link = df.loc[i, 'upforgrabs__link']
+                sc = df.loc[i, 'stats__issue-count']
                 printed_names.append(name)
-                res.append({'name': name, 'tags': tags, 'link': link, 'desc': desc}) # Add name to printed names list
-        return jsonify(msg=res, status="success");
+                res.append({'name': name, 'tags': tags, 'link': link, 'desc': desc,
+                           'upforgrabs__link': upforgrabs__link, 'stats__issue-count': sc})  # Add name to printed names list
+        return jsonify(msg=res, status="success")
     else:
         return jsonify(msg="No matching repository found", status="error")
 
